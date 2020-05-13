3cd424a1dfac4a0b9d95e967fa716a9829d6a803
haykalox <64409539+haykalox@users.noreply.github.com>
2020-05-13 14:27:58 +0100

    working app
    Reader CSV with maven spark

--- src/main/scala/com/haykel/app/App.scala
+++ src/main/scala/com/haykel/app/App.scala
@@ -1,8 +1,18 @@
 package com.haykel.app
 
-object App {
-  def main(args: Array[String]) {
-    println("Hello, world!")
-  }
+
+object App extends App {
+  val spark = org.apache.spark.sql.SparkSession.builder
+    .master("local")
+    .appName("Spark CSV Reader")
+    .getOrCreate;
+
+  val df = spark.read
+    .format("csv")
+    .option("header", "true") //first line in file has headers
+    .option("mode", "DROPMALFORMED")
+    .load("C:\\Users\\haykalo\\IdeaProjects\\Hello-world\\src\\main\\resources\\test.csv")
+
+  df.show()
 
 }
